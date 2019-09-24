290352eb1afcbd67934ea770ce022d432163591b
superkldd <55684964+superkldd@users.noreply.github.com>
2019-09-25 01:21:22 +0900

    Add files via upload

---
+++ mochapoo.py
@@ -0,0 +1,33 @@
+import discord
+import requests
+import asyncio
+from json import loads
+
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(client.user.name)
+    print("ready")
+    game = discord.Game("열심히")
+    await client.change_presence(status=discord.Status.online, activity=game)
+    twitch = "mochapoo"
+    name = "모카똥"
+    channel = client.get_channel(357188444872507396)
+    a = 0
+    while True:
+        headers = {'Client-ID': '6vcpx7k4l6oiokev21orjk5mcmh2ej'}
+        response = requests.get("https://api.twitch.tv/helix/streams?user_login=" + twitch, headers=headers)
+        try:
+            if loads(response.text)['data'][0]['type'] == 'live' and a == 0:
+                await channel.send("```" + name + " 님이 방송을 시작하였습니다.알로알로!!" + "```")
+                a = 1
+        except:
+            a = 0
+        await asyncio.sleep(3)
+
+
+client.run('NjI2MDgzMzY5OTE4Mzk4NTA0.XYo8-g.4GM7HCQwZkbL90KuN7hf9dpx41s')
+
